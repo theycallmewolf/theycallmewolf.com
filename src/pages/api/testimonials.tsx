e42a8f01b4e48d0063797f8074a41c56357ed0ad
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-01 00:51:50 +0100

    adds testimonials api endpoint

---
+++ src/pages/api/testimonials.tsx
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse): void => {
+  const testimonials = [
+    {
+      id: 1,
+      quote:
+        'Mr. Wolf Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. ',
+      name: 'Elon Musk',
+      jobDescription: 'SpaceX CEO',
+      date: '11 May 2021'
+    },
+    {
+      id: 2,
+      quote:
+        'Mr. Wolf Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. ',
+      name: 'Sam Jackson',
+      jobDescription: 'American Actor',
+      date: '11 May 2021'
+    }
+  ];
+
+  return response.json(testimonials);
+};
+
+// http://localhost:3000/api/testimonials
