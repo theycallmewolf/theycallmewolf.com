6ec649341ef2531e701133dd9626cb92b8c05bb1
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-01 22:23:55 +0100

    header component version 2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,49 +11,34 @@ export const Header: React.FC = () => {
   return (
     <header className={`${styles.container} ${isOpen && styles.open}`}>
       <SVGSprite />
-      <div className={styles.background}>
-        <div></div>
-        <svg viewBox="0 0 1366 711">
-          <use href="#wire" />
-        </svg>
-      </div>
-      <div className={styles.content}>
+      <div className={styles.navigation}>
         <Nav />
       </div>
       <div className={styles.header}>
-        <svg
-          className={styles.headerBackground}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1366 90"
-          preserveAspectRatio="none">
-          <path d="M0 0v63h345v27h565V73h456V0z" />
-        </svg>
-        <div className={styles.content}>
-          <Link href="/">
-            <a className={`${styles.logo} ${isOpen && styles.open}`}>
-              <svg viewBox="0 0 202 190">
-                <use href="#wolf" />
+        <Link href="/">
+          <a className={`${styles.logo} ${isOpen && styles.open}`}>
+            <svg viewBox="0 0 202 190">
+              <use href="#wolf" />
+            </svg>
+          </a>
+        </Link>
+        <div className={styles.buttonContainer}>
+          <button className={styles.toggle} aria-label="toggle light/dark mode"></button>
+          <button
+            className={styles.menu}
+            aria-label="menu button"
+            onClick={() => setIsOpen(!isOpen)}>
+            {!isOpen ? 'menu' : 'close'}
+            {!isOpen ? (
+              <svg>
+                <use href="#i-burger" />
               </svg>
-            </a>
-          </Link>
-          <div className={styles.buttonContainer}>
-            <button
-              className={styles.menu}
-              aria-label="menu button"
-              onClick={() => setIsOpen(!isOpen)}>
-              {!isOpen ? 'menu' : 'close'}
-              {!isOpen ? (
-                <svg>
-                  <use href="#i-burger" />
-                </svg>
-              ) : (
-                <svg>
-                  <use href="#i-close" />
-                </svg>
-              )}
-            </button>
-            <button className={styles.toggle} aria-label="toggle light/dark mode"></button>
-          </div>
+            ) : (
+              <svg>
+                <use href="#i-close" />
+              </svg>
+            )}
+          </button>
         </div>
       </div>
     </header>
