d811488e5b001ed3046ab5158c8ac3677cc03c0c
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-02 20:38:23 +0100

    fix: intro area on Blog component

--- src/components/sections/Blog/index.tsx
+++ src/components/sections/Blog/index.tsx
@@ -17,12 +17,18 @@ export function Blog({ posts }: BlogProps): JSX.Element {
   return (
     <section className={styles.section}>
       <div className={styles.intro}>
-        <h2>Wolf Bytes</h2>
-        <small>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
-          elit.
-        </small>
-        <Button genre="outline">More Bytes</Button>
+        <div>
+          <h2>Wolf Bytes</h2>
+          <p>
+            <small>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem
+              nec elit.
+            </small>
+          </p>
+        </div>
+        <div>
+          <Button genre="outline">More Bytes</Button>
+        </div>
       </div>
       {posts.map((post) => (
         <BlogCard key={post.id} title={post.title} lead={post.lead} />
