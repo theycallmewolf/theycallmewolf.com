1408ee4fd853feabcded868c9ab7fe2583c7ea8d
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-03 15:54:23 +0100

    adds date to blog card

--- src/components/elements/Cards/BlogCard/index.tsx
+++ src/components/elements/Cards/BlogCard/index.tsx
@@ -4,11 +4,13 @@ import styles from './styles.module.scss';
 type BlogCardProps = {
   title: string;
   lead: string;
+  date: string;
 };
 
-export function BlogCard({ title, lead }: BlogCardProps): JSX.Element {
+export function BlogCard({ title, lead, date }: BlogCardProps): JSX.Element {
   return (
     <div className={styles.container}>
+      <span className={styles.date}>{date}</span>
       <h3>{title}</h3>
       <p>{lead}</p>
       <Button genre="fill">Read</Button>
