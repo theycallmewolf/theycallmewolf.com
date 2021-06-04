5ad41540093e015b1f4cf28a01edb21aa2849b48
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-04 12:08:29 +0100

    adds isSelectes prop to NavLink component

--- src/components/elements/Nav/NavLink/index.tsx
+++ src/components/elements/Nav/NavLink/index.tsx
@@ -3,10 +3,11 @@ import styles from './styles.module.scss';
 interface NavLinkProps {
   link: string;
   label: string;
+  isSelected?: boolean;
 }
-export function NavLink({ link, label }: NavLinkProps): JSX.Element {
+export function NavLink({ link, label, isSelected }: NavLinkProps): JSX.Element {
   return (
-    <a href={link} className={styles.link}>
+    <a href={link} className={`${styles.link} ${isSelected && styles.selected}`}>
       {label}
     </a>
   );
--- src/components/elements/Nav/index.tsx
+++ src/components/elements/Nav/index.tsx
@@ -8,7 +8,7 @@ export function Nav({ adicionalClassName }: NavProps): JSX.Element {
   return (
     <nav className={`${styles.nav} ${adicionalClassName && adicionalClassName}`}>
       <NavLink link="/" label="skill set" />
-      <NavLink link="/" label="services" />
+      <NavLink link="/" label="services" isSelected />
       <NavLink link="/" label="professional" />
       <NavLink link="/" label="academic" />
     </nav>
