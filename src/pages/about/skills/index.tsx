c0a59d0cac0787d6b02c6e6fe45def6c61633a7f
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-06 12:17:26 +0100

    adds static props for skills page

--- src/pages/about/skills/index.tsx
+++ src/pages/about/skills/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { AboutCard } from '../../../components/elements/Cards/AboutCard';
@@ -7,7 +8,37 @@ import { Intro } from '../../../components/elements/Intro';
 import { Header } from '../../../components/layouts/Header';
 import styles from './styles.module.scss';
 
-export default function About(): JSX.Element {
+type Link = {
+  id: number;
+  link: string;
+  label: string;
+};
+
+type IntroData = {
+  title: string;
+  lead: string;
+  linkList: Link[];
+};
+
+type Graph = {
+  id: number;
+  title: string;
+  percentage: number;
+};
+
+type CardsData = {
+  id: number;
+  title: string;
+  description: string;
+  graphs: Graph[];
+};
+
+interface AboutProps {
+  intro: IntroData;
+  cards: CardsData[];
+}
+
+export default function About({ intro, cards }: AboutProps): JSX.Element {
   return (
     <main className={styles.main}>
       <Head>
@@ -17,63 +48,178 @@ export default function About(): JSX.Element {
       <Header />
       <main>
         <Cover imageURL="/assets/img/cover-about.jpg" />
-        <Intro />
+        <Intro {...intro} />
         <div className={styles.cardList}>
-          <AboutCard adicionalClass={styles.card}>
-            <h2>front-end</h2>
-            <p>
-              Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id
-              elit. Cras justo odio, dapibus ac facilisis in
-            </p>
-            <div className={styles.graphList}>
-              <Graph title="HTML 5" percentage={80} />
-              <Graph title="CSS 3" percentage={80} />
-              <Graph title="JavaScript" percentage={60} />
-              <Graph title="ReactJS" percentage={50} />
-              <Graph title="NextJS" percentage={50} />
-              <Graph title="Sass" percentage={60} />
-            </div>
-          </AboutCard>
-          <AboutCard adicionalClass={styles.card}>
-            <h2>back-end</h2>
-            <p>
-              Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id
-              elit. Cras justo odio, dapibus ac facilisis in
-            </p>
-            <div className={styles.graphList}>
-              <Graph title="NodeJS" percentage={80} />
-              <Graph title="PHP" percentage={80} />
-            </div>
-          </AboutCard>
-          <AboutCard adicionalClass={styles.card}>
-            <h2>other</h2>
-            <p>
-              Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id
-              elit. Cras justo odio, dapibus ac facilisis in
-            </p>
-            <div className={styles.graphList}>
-              <Graph title="React Native" percentage={80} />
-              <Graph title="Git + Git Hub" percentage={80} />
-              <Graph title="TypeScript" percentage={60} />
-              <Graph title="yarn / npm" percentage={50} />
-            </div>
-          </AboutCard>
-          <AboutCard adicionalClass={styles.card}>
-            <h2>software</h2>
-            <p>
-              Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id
-              elit. Cras justo odio, dapibus ac facilisis in
-            </p>
-            <div className={styles.graphList}>
-              <Graph title="Visual Studio Code" percentage={80} />
-              <Graph title="Adobe XD" percentage={80} />
-              <Graph title="Adobe Illustrator" percentage={60} />
-              <Graph title="Adobe Photoshop" percentage={50} />
-              <Graph title="Insomnia" percentage={50} />
-            </div>
-          </AboutCard>
+          {cards.map((card) => (
+            <AboutCard key={card.id} adicionalClass={styles.card}>
+              <h2>{card.title}</h2>
+              <p>{card.description}</p>
+              <div className={styles.graphList}>
+                {card.graphs.map((graph) => (
+                  <Graph title={graph.title} percentage={graph.percentage} key={graph.id} />
+                ))}
+              </div>
+            </AboutCard>
+          ))}
         </div>
       </main>
     </main>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const intro = {
+    title: 'about',
+    lead: 'Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+    linkList: [
+      {
+        id: 1,
+        link: '/about/skills',
+        label: 'skills'
+      },
+      {
+        id: 2,
+        link: '/about/services',
+        label: 'services'
+      },
+      {
+        id: 3,
+        link: '/about/pro',
+        label: 'pro'
+      },
+      {
+        id: 4,
+        link: '/about/academic',
+        label: 'academic'
+      }
+    ]
+  };
+
+  const cards: CardsData[] = [
+    {
+      id: 1,
+      title: 'front-end',
+      description:
+        'Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in',
+      graphs: [
+        {
+          id: 1,
+          title: 'HTML 5',
+          percentage: 80
+        },
+        {
+          id: 2,
+          title: 'CSS 3',
+          percentage: 80
+        },
+        {
+          id: 3,
+          title: 'JavaScript',
+          percentage: 80
+        },
+        {
+          id: 4,
+          title: 'ReactJS',
+          percentage: 80
+        },
+        {
+          id: 5,
+          title: 'NextJS',
+          percentage: 80
+        },
+        {
+          id: 6,
+          title: 'Sass',
+          percentage: 80
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'back-end',
+      description:
+        'Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in',
+      graphs: [
+        {
+          id: 1,
+          title: 'NodeJS',
+          percentage: 80
+        },
+        {
+          id: 2,
+          title: 'PHP',
+          percentage: 80
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'other',
+      description:
+        'Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in',
+      graphs: [
+        {
+          id: 1,
+          title: 'React Native',
+          percentage: 80
+        },
+        {
+          id: 2,
+          title: 'Git + Git Hub',
+          percentage: 80
+        },
+        {
+          id: 3,
+          title: 'TypeScript',
+          percentage: 80
+        },
+        {
+          id: 4,
+          title: 'yarn / npm',
+          percentage: 80
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'software',
+      description:
+        'Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in',
+      graphs: [
+        {
+          id: 1,
+          title: 'Visual Studio Code',
+          percentage: 80
+        },
+        {
+          id: 2,
+          title: 'Adobe XD',
+          percentage: 80
+        },
+        {
+          id: 3,
+          title: 'Adobe Illustrator',
+          percentage: 80
+        },
+        {
+          id: 4,
+          title: 'Adobe Photoshop',
+          percentage: 80
+        },
+        {
+          id: 5,
+          title: 'Insomnia',
+          percentage: 80
+        }
+      ]
+    }
+  ];
+
+  return {
+    props: {
+      intro,
+      cards
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  };
+};
