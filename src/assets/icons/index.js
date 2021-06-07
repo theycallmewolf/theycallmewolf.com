ffd6407dbfffe8bd7df85981aab039ed4f074096
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-07 21:14:37 +0100

    refactor Wolf icon component

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,4 +3,4 @@ export { default as IArrowRed } from './IArrowRed';
 export { default as IArrowWhite } from './IArrowWhite';
 export { default as ICross } from './ICross';
 export { default as IMenu } from './IMenu';
-export { default as Wolf } from './Wolf';
+export { default as Wolf } from './IWolf';
