6c2616a68eabe056090bf2949adf0675ee960ab8
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-07 22:28:58 +0100

    extends react html attrs to graphic card component

--- src/components/elements/Cards/GraphicCard/index.tsx
+++ src/components/elements/Cards/GraphicCard/index.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import { TangramCard } from '../../../../assets/tangrams';
 import styles from './styles.module.scss';
 
-type GraphicCardProps = {
-  children: React.ReactNode;
+interface GraphicCardProps extends React.HTMLAttributes<HTMLElement> {
   adicionalClass?: string;
-};
+}
 
-export function GraphicCard({ children, adicionalClass }: GraphicCardProps): JSX.Element {
+export function GraphicCard({
+  adicionalClass = '',
+  children,
+  ...rest
+}: GraphicCardProps): JSX.Element {
   return (
-    <div className={`${styles.container} ${adicionalClass && adicionalClass}`}>
+    <div className={`${styles.container} ${adicionalClass}`} {...rest}>
       <div className={styles.content}>{children}</div>
       <TangramCard />
     </div>
