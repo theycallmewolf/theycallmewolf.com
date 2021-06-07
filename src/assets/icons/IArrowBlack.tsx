88cd8120e4c8d07df9986c50cb9f56cadf28f11c
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-07 22:22:02 +0100

    removes unused icon components

--- src/assets/icons/IArrowBlack.tsx
+++
@@ -1,9 +0,0 @@
-function SvgIArrowBlack(): JSX.Element {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22">
-      <path d="M15 13l-9 9M6 0l11 11" stroke="#111517" strokeMiterlimit={10} />
-    </svg>
-  );
-}
-
-export default SvgIArrowBlack;
--- src/assets/icons/IArrowWhite.tsx
+++
@@ -1,9 +0,0 @@
-function SvgIArrowWhite(): JSX.Element {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22">
-      <path d="M15 13l-9 9M6 0l11 11" stroke="#f5f5f5" strokeMiterlimit={10} />
-    </svg>
-  );
-}
-
-export default SvgIArrowWhite;
