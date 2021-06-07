6a5e526384d0d8a552ccf2448030474bc7b40e3e
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-07 21:50:43 +0100

    refactor: adds typescript to icons function components

--- src/assets/icons/ICross.tsx
+++ src/assets/icons/ICross.tsx
@@ -1,8 +1,6 @@
-import * as React from 'react';
-
-function SvgICross(props) {
+function SvgICross(): JSX.Element {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22">
       <path
         d="M10 12L.3 21.7M21.7.3L11.3 10.7m10.4 11L.3.3"
         strokeMiterlimit={10}
--- src/assets/icons/IMenu.tsx
+++ src/assets/icons/IMenu.tsx
@@ -1,8 +1,6 @@
-import * as React from 'react';
-
-function SvgIMenu(props) {
+function SvgIMenu(): JSX.Element {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22">
       <path d="M0 11.5h22M0 .5h10.6m0 21H22" strokeMiterlimit={10} strokeWidth={2} />
     </svg>
   );
