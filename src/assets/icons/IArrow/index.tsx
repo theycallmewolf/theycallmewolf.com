0150cd6737c8113a7afc430debc95efbf797fa13
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-07 21:50:56 +0100

    adds IArrow component

---
+++ src/assets/icons/IArrow/index.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss';
+
+interface IArrowProps {
+  color?: 'white' | 'red' | 'black';
+}
+function IArrow({ color }: IArrowProps): JSX.Element {
+  console.log(color);
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" className={styles.container}>
+      <path
+        className={`${styles.element} ${color === 'white' ? styles.white : ''} ${
+          color === 'red' ? styles.red : ''
+        } ${color === 'black' ? styles.black : ''}`}
+        d="M15 13l-9 9M6 0l11 11"
+        strokeMiterlimit={10}
+      />
+    </svg>
+  );
+}
+
+export default IArrow;
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as IArrowBlack } from './IArrowBlack';
+export { default as IArrowRed } from './IArrowRed';
+export { default as IArrowWhite } from './IArrowWhite';
+export { default as ICross } from './ICross';
+export { default as IMenu } from './IMenu';
+export { default as Wolf } from './IWolf';
+export { default as iArrow } from './IArrow';
