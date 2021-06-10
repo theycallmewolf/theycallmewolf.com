49e7eb54897a9c52265c489fe6bb9adef5c3a95e
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-10 15:03:28 +0100

    adds icon 'plus'

---
+++ src/assets/icons/IPlus.tsx
@@ -0,0 +1,11 @@
+type SvgIPlusProps = React.HTMLAttributes<HTMLOrSVGElement>;
+
+function SvgIPlus({ ...rest }: SvgIPlusProps): JSX.Element {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" {...rest}>
+      <path strokeMiterlimit={10} d="M9.6 11H0m22 0H11.4M11 22V0" />
+    </svg>
+  );
+}
+
+export default SvgIPlus;
--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { default as ICross } from './ICross';
 export { default as IMenu } from './IMenu';
 export { default as Wolf } from './IWolf';
 export { default as iArrow } from './IArrow';
+export { default as IPlus } from './IPlus';
