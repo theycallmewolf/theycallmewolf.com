ad0a13dac7c08c0cd927816aec1e61fd6c397218
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-12 12:54:58 +0100

    get works from prismic

--- src/pages/work/[slug].tsx
+++ src/pages/work/[slug].tsx
@@ -7,7 +7,7 @@ import { CardBody, CardHeader, DefaultCard } from '../../components/elements/Car
 import { CustomLink } from '../../components/elements/Link';
 import ListPage from '../../components/layouts/ListPage';
 import { useTheme } from '../../hooks/useTheme';
-import { formatDate } from '../../utils';
+import { getContent } from '../../services/prismic';
 import styles from './styles.module.scss';
 
 type Link = { id: number; link: string; label: string };
@@ -19,20 +19,29 @@ type ProjectData = {
   slug: string;
   title: string;
   description: string;
-  image: string | null;
-  slug: string;
-  updateDate: string;
-  publishDate: string;
-  repository: string | null;
-  repositoryApi: string | null;
-  url: string | null;
-  specs: string[];
-  team: TeamData[] | null;
+  image_large: string;
+  image_small: string;
+  projectDate: string;
 };
+
+// type CodeProjectsData = {
+//   id: number;
+//   title: string;
+//   description: string;
+//   image: string | null;
+//   slug: string;
+//   updateDate: string;
+//   publishDate: string;
+//   repository: string | null;
+//   repositoryApi: string | null;
+//   url: string | null;
+//   specs: string[];
+//   team: TeamData[] | null;
+// };
 
 interface WorkProps {
   intro: IntroData;
-  cards: CodeProjectsData[];
+  cards: ProjectData[];
 }
 
 export default function Work({ intro, cards }: WorkProps): JSX.Element {
@@ -52,23 +61,26 @@ export default function Work({ intro, cards }: WorkProps): JSX.Element {
       slug={slug}
       pageDescription="...">
       {cards.map((project) => (
-        <DefaultCard key={project.id} customClass={styles.card} imageFilter={project.image !== ''}>
+        <DefaultCard key={project.id} customClass={styles.card}>
           <CardHeader>
-            {project.image !== '' ? (
-              <img src={project.image} alt={project.title} />
+            {project.image_small !== '' ? (
+              <picture>
+                <source srcSet={project.image_large} media="(min-width: 425px)" />
+                <img src={project.image_small} alt={project.title} />
+              </picture>
             ) : (
               <TangramCard customClass={styles.placeholder} />
             )}
           </CardHeader>
           <CardBody>
-            <span className={styles.date}>{project.publishDate}</span>
+            <span className={styles.date}>{project.projectDate}</span>
             <h2>{project.title}</h2>
             <p>{project.description}</p>
-            <ul>
+            {/* <ul>
               {project.specs.map((spec) => {
                 <li>{spec}</li>;
               })}
-            </ul>
+            </ul> */}
             <CustomLink
               label="more"
               href={`/work/code/${project.slug}`}
@@ -112,134 +124,133 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     ]
   };
 
-  function getCodeProjects(): CodeProjectsData[] {
-    return [
-      {
-        id: 1,
-        title: 'watch',
-        description:
-          '"Excuse me! Do you have the time?!" A simple project to practice my CSS and JS skills, and also learn a bit about webpack.',
-        slug: 'watch',
-        image:
-          'https://images.unsplash.com/photo-1512853378841-2c404110a4f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80',
-        publishDate: '2021-02-13T01:38:03Z',
-        updateDate: '2021-06-01T08:38:14Z',
-        repository: 'https://github.com/theycallmewolf/wolf-watch',
-        repositoryApi: 'https://api.github.com/repos/theycallmewolf/wolf-watch',
-        url: 'https://watch.theycallmewolf.pt/',
-        specs: ['scss', 'javascript', 'webpack', 'html'],
-        team: null
-      },
-      {
-        id: 2,
-        title: 'HUThome',
-        description:
-          'a record time project to keep delivering fitness content during quarentine, delivering live and on-demand workouts',
-        slug: 'huthome',
-        image:
-          'https://images.unsplash.com/photo-1465505486239-73576daab293?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1951&q=80',
-        publishDate: '2020-04-28T01:38:03Z',
-        updateDate: '',
-        repository: null,
-        repositoryApi: null,
-        url: 'https://www.fitnesshut.pt/hut-home/',
-        specs: ['html', 'css', 'javascript', 'php'],
-        team: [
-          {
-            name: 'Nuno Bengalito',
-            url: 'https://github.com/nbengalito'
-          }
-        ]
-      },
-      {
-        id: 3,
-        title: 'Onlive',
-        description:
-          'onlive, a website for booking and assist online live music concerts ( final project for react.js course module at etic_ )',
-        slug: 'onlive',
-        image:
-          'https://images.unsplash.com/photo-1496888057897-8a25eef593f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80',
-        publishDate: '2020-06-13T11:34:34Z',
-        updateDate: '2020-10-23T10:28:34Z',
-        repository: 'https://github.com/theycallmewolf/onlive',
-        repositoryApi: 'https://api.github.com/repos/theycallmewolf/onlive',
-        url: null,
-        specs: ['reactjs', 'php', 'javascript', 'html5', 'cssmodules-react'],
-        team: null
-      },
-      {
-        id: 4,
-        title: 'Hut reception',
-        description: 'web app for Fitness Hut club reception area tablest',
-        slug: 'hut-reception',
-        image:
-          'https://images.unsplash.com/photo-1540908300676-b00e9a003736?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80',
-        publishDate: '2020-12-23T14:18:00Z',
-        updateDate: '2021-05-11T13:12:00Z',
-        repository: 'https://github.com/theycallmewolf/hut-reception',
-        repositoryApi: 'https://api.github.com/repos/theycallmewolf/hut-reception',
-        url: null,
-        specs: [
-          'reactjs',
-          'html',
-          'javascript',
-          'yup',
-          'styled-components',
-          'crypto-js',
-          'axios',
-          'public-ip',
-          'qrcode.react',
-          'uuid'
-        ],
-        team: null
-      },
-      {
-        id: 5,
-        title: 'wheel of fortune',
-        description: 'A wheel of fortune developed just to have fun with javascript',
-        slug: 'wheel-of-fortune',
-        image: '',
-        url: 'https://wheeloffortune.theycallmewolf.pt/',
-        repository: 'https://github.com/theycallmewolf/wheel-of-fortune',
-        repositoryApi: 'https://api.github.com/repos/theycallmewolf/wheel-of-fortune',
-        specs: ['javascript', 'html', 'css'],
-        publishDate: '2020-07-23T18:16:12Z',
-        updateDate: '2021-02-13T18:54:25Z',
-        team: null
-      },
-      {
-        id: 6,
-        title: 'Go Barber',
-        description: '',
-        image:
-          'https://images.unsplash.com/photo-1548053279-649e31d44b03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80',
-        publishDate: '2020-11-21T13:42:00Z',
-        updateDate: '2021-02-09T14:51:56Z',
-        url: 'https://gobarber.theycallmewolf.pt/',
-        repository: 'https://github.com/theycallmewolf/gostack-gobarber-web',
-        repositoryApi: 'https://api.github.com/repos/theycallmewolf/gostack-gobarber-web',
-        team: null,
-        slug: 'go-barber',
-        specs: ['react', 'javascript', 'nodejs', 'react native']
-      }
-    ];
+  async function getProjectData(): Promise<ProjectData[]> {
+    const projectsResponse = await getContent({
+      type: 'projects',
+      fields: ['title', 'imageurl', 'caption']
+    });
+
+    return projectsResponse.map((project) => {
+      const { slug, title, description, images, dates, type, id } = project;
+      const { slider } = images;
+      const { image_large, image_small } = slider;
+      const { projectDate } = dates;
+      return {
+        id,
+        type,
+        slug,
+        title,
+        description,
+        image_large,
+        image_small,
+        projectDate
+      };
+    });
   }
 
-  const projects = getCodeProjects();
-  cards = '';
-  cards = projects.map((project) => {
-    const { id, title, description, image, slug, specs, publishDate } = project;
+  const projects: ProjectData[] = await getProjectData();
 
-    return {
-      id,
-      title,
-      description,
-      image,
-      slug,
-      specs,
-      publishDate: formatDate(publishDate)
-    };
-  });
+  const codeProjects = projects.filter((project) => project.type === 'code');
+  const designProjects = projects.filter((project) => project.type === 'design');
+  const illustrationProjects = projects.filter((project) => project.type === 'illustration');
+  const otherProjects = projects.filter((project) => project.type === 'other');
+
+  switch (slug) {
+    case 'code':
+      cards = codeProjects;
+      break;
+
+    case 'design':
+      cards = designProjects;
+      break;
+
+    case 'illustration':
+      cards = illustrationProjects;
+      break;
+
+    case 'other':
+      cards = otherProjects;
+      break;
+
+    default:
+      cards = codeProjects;
+      break;
+  }
+  // function getCodeProjects(): CodeProjectsData[] {
+  //   return [
+  //     {
+  //       id: 3,
+  //       title: 'Onlive',
+  //       description:
+  //         'onlive, a website for booking and assist online live music concerts ( final project for react.js course module at etic_ )',
+  //       slug: 'onlive',
+  //       image:
+  //         'https://images.unsplash.com/photo-1496888057897-8a25eef593f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80',
+  //       publishDate: '2020-06-13T11:34:34Z',
+  //       updateDate: '2020-10-23T10:28:34Z',
+  //       repository: 'https://github.com/theycallmewolf/onlive',
+  //       repositoryApi: 'https://api.github.com/repos/theycallmewolf/onlive',
+  //       url: null,
+  //       specs: ['reactjs', 'php', 'javascript', 'html5', 'cssmodules-react'],
+  //       team: null
+  //     },
+  //     {
+  //       id: 4,
+  //       title: 'Hut reception',
+  //       description: 'web app for Fitness Hut club reception area tablest',
+  //       slug: 'hut-reception',
+  //       image:
+  //         'https://images.unsplash.com/photo-1540908300676-b00e9a003736?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80',
+  //       publishDate: '2020-12-23T14:18:00Z',
+  //       updateDate: '2021-05-11T13:12:00Z',
+  //       repository: 'https://github.com/theycallmewolf/hut-reception',
+  //       repositoryApi: 'https://api.github.com/repos/theycallmewolf/hut-reception',
+  //       url: null,
+  //       specs: [
+  //         'reactjs',
+  //         'html',
+  //         'javascript',
+  //         'yup',
+  //         'styled-components',
+  //         'crypto-js',
+  //         'axios',
+  //         'public-ip',
+  //         'qrcode.react',
+  //         'uuid'
+  //       ],
+  //       team: null
+  //     },
+  //     {
+  //       id: 5,
+  //       title: 'wheel of fortune',
+  //       description: 'A wheel of fortune developed just to have fun with javascript',
+  //       slug: 'wheel-of-fortune',
+  //       image: '',
+  //       url: 'https://wheeloffortune.theycallmewolf.pt/',
+  //       repository: 'https://github.com/theycallmewolf/wheel-of-fortune',
+  //       repositoryApi: 'https://api.github.com/repos/theycallmewolf/wheel-of-fortune',
+  //       specs: ['javascript', 'html', 'css'],
+  //       publishDate: '2020-07-23T18:16:12Z',
+  //       updateDate: '2021-02-13T18:54:25Z',
+  //       team: null
+  //     },
+  //     {
+  //       id: 6,
+  //       title: 'Go Barber',
+  //       description: '',
+  //       image:
+  //         'https://images.unsplash.com/photo-1548053279-649e31d44b03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80',
+  //       publishDate: '2020-11-21T13:42:00Z',
+  //       updateDate: '2021-02-09T14:51:56Z',
+  //       url: 'https://gobarber.theycallmewolf.pt/',
+  //       repository: 'https://github.com/theycallmewolf/gostack-gobarber-web',
+  //       repositoryApi: 'https://api.github.com/repos/theycallmewolf/gostack-gobarber-web',
+  //       team: null,
+  //       slug: 'go-barber',
+  //       specs: ['react', 'javascript', 'nodejs', 'react native']
+  //     }
+  //   ];
+  // }
 
   return {
     props: {
