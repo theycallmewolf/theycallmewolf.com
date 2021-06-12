1f89c7c3b7cb523a5843cc4387e8ebdc0990e9da
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-12 11:53:27 +0100

    adds icon to CustomLink component

--- src/components/elements/Link/index.tsx
+++ src/components/elements/Link/index.tsx
@@ -1,18 +1,27 @@
 import Link from 'next/link';
 
+import { IPlus } from '../../../assets/icons';
 import styles from './styles.module.scss';
 
 interface CustomLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   href: string;
-  label: string;
+  label?: string;
   customClass?: string;
+  hasIcon?: boolean;
 }
 
-export function CustomLink({ customClass, href, label, ...rest }: CustomLinkProps): JSX.Element {
+export function CustomLink({
+  customClass,
+  href,
+  label,
+  hasIcon,
+  ...rest
+}: CustomLinkProps): JSX.Element {
   return (
     <Link href={href}>
-      <a className={`${styles.link} ${customClass ?? ''}`} {...rest}>
+      <a className={`${styles.link} ${customClass ?? ''} ${hasIcon ? styles.icon : ''}`} {...rest}>
         {label}
+        {hasIcon && <IPlus />}
       </a>
     </Link>
   );
