53ce19b55d345ca261c20dffb4da22f00da45a07
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-21 18:09:44 +0100

    fix icon import on index

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,7 @@
 export { default as ICross } from './ICross';
 export { default as IMenu } from './IMenu';
 export { default as Wolf } from './IWolf';
-export { default as iArrow } from './IArrow';
+export { default as IArrow } from './IArrow';
 export { default as IPlus } from './IPlus';
 export { default as IMinus } from './IMinus';
 export { default as IChat } from './IChat';
