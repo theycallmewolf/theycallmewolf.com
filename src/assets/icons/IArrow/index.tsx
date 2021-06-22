540470f4b864159d491248ef4f8228ced8be7c7e
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-22 15:22:40 +0100

    fix Arrow icon color class attribution

--- src/assets/icons/IArrow/index.tsx
+++ src/assets/icons/IArrow/index.tsx
@@ -3,13 +3,18 @@ import styles from './styles.module.scss';
 interface IArrowProps extends React.HTMLAttributes<HTMLOrSVGElement> {
   color?: 'white' | 'red' | 'black';
 }
-function IArrow({ color }: IArrowProps): JSX.Element {
+
+function IArrow({ color = 'white' }: IArrowProps): JSX.Element {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" className={styles.container}>
       <path
-        className={`${styles.element} ${color === 'white' ? styles.white : ''} ${
-          color === 'red' ? styles.red : ''
-        } ${color === 'black' ? styles.black : ''}`}
+        className={
+          styles.element +
+          ' ' +
+          (color === 'white' ? styles.white : '') +
+          (color === 'red' ? styles.red : '') +
+          (color === 'black' ? styles.black : '')
+        }
         d="M15 13l-9 9M6 0l11 11"
         strokeMiterlimit={10}
       />
