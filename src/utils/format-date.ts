1417d0997ed1d567d2aa2c9a144c2268444b5c92
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-23 13:39:59 +0100

    updates format-date utill from 'pt-PT' to 'en-EN'

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,5 +1,5 @@
 export const formatDate = (date: string): string =>
-  new Date(date).toLocaleDateString('pt-PT', {
+  new Date(date).toLocaleDateString('en-EN', {
     day: '2-digit',
     month: 'long',
     year: 'numeric'
