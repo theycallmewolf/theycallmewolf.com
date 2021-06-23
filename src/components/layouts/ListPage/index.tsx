f111807385a972241b34f7d57bd2ee297e311b1d
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-23 18:42:17 +0100

    interface resedign for 'ListPage' component

--- src/components/layouts/ListPage/index.tsx
+++ src/components/layouts/ListPage/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 
+import { TangramCover } from '../../../assets/tangrams';
 import { ListPageProps } from '../../../types';
-import { Cover } from '../../elements/Cover';
-import { Intro } from '../../sections/Intro';
+import { Nav } from '../../elements/Nav';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import styles from './styles.module.scss';
@@ -22,15 +22,25 @@ export default function ListPage({
         <title>they call me wolf | ${pageTitle}</title>
         <meta name="description" content={pageDescription} />
       </Head>
-      <main className={styles.main}>
+      <main>
         <Header />
-        <Cover imageURL={imageURL} />
-        <Intro link_list={link_list} title={intro[0].title} lead={intro[0].lead} />
-        <div className={styles.container}>
-          <div className={`${styles.cardList} ${slug === 'activity' && styles.col2}`}>
-            {children}
+        <aside className={styles.aside}>
+          <div
+            className={styles.cover}
+            style={{
+              backgroundImage: `url(${imageURL})`
+            }}>
+            <TangramCover />
           </div>
-        </div>
+          <div className={styles.content}>
+            <h1>{intro[0].title}</h1>
+            <p className="lead">{intro[0].lead}</p>
+            <Nav customClass={styles.nav} link_list={link_list} />
+          </div>
+        </aside>
+        <section className={`${styles.cardList} ${slug === 'activity' ? styles.col2 : ''}`}>
+          {children}
+        </section>
       </main>
       <Footer />
     </>
