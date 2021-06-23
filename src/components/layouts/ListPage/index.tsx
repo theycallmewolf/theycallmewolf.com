ceaa3bffb6a9de1e14db207beabf7c11c5ffaca8
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-23 15:14:18 +0100

    adds footer to 'ListPage' layout

--- src/components/layouts/ListPage/index.tsx
+++ src/components/layouts/ListPage/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { ListPageProps } from '../../../types';
 import { Cover } from '../../elements/Cover';
 import { Intro } from '../../sections/Intro';
+import { Footer } from '../Footer';
 import { Header } from '../Header';
 import styles from './styles.module.scss';
 
@@ -25,8 +26,13 @@ export default function ListPage({
         <Header />
         <Cover imageURL={imageURL} />
         <Intro link_list={link_list} title={intro[0].title} lead={intro[0].lead} />
-        <div className={`${styles.cardList} ${slug === 'activity' && styles.col2}`}>{children}</div>
+        <div className={styles.container}>
+          <div className={`${styles.cardList} ${slug === 'activity' && styles.col2}`}>
+            {children}
+          </div>
+        </div>
       </main>
+      <Footer />
     </>
   );
 }
