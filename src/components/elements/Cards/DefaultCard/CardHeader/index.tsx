73b92329a32451e27010505bec4489e93c691217
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-24 00:46:56 +0100

    fix 'customClass' prop on 'CardHeader'

--- src/components/elements/Cards/DefaultCard/CardHeader/index.tsx
+++ src/components/elements/Cards/DefaultCard/CardHeader/index.tsx
@@ -3,9 +3,9 @@ import styles from './styles.module.scss';
 interface CardHeaderProps extends React.HTMLAttributes<HTMLElement> {
   customClass?: string;
 }
-export function CardHeader({ customClass, children, ...rest }: CardHeaderProps): JSX.Element {
+export function CardHeader({ customClass = '', children, ...rest }: CardHeaderProps): JSX.Element {
   return (
-    <div className={`${styles.cardHeader} ${customClass ?? ''}`} {...rest}>
+    <div className={`${styles.cardHeader} ${customClass}`} {...rest}>
       {children}
       <div className={styles.clip}></div>
     </div>
