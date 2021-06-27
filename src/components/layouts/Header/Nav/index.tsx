ff967e30e2feba4f5d0152af9cc961ee83bf2380
theycallmewolf <bruno.m.lobato@gmail.com>
2021-06-27 03:22:08 +0100

    change the 'Header NavLink' order

--- src/components/layouts/Header/Nav/index.tsx
+++ src/components/layouts/Header/Nav/index.tsx
@@ -5,8 +5,8 @@ export function Nav(): JSX.Element {
   return (
     <nav className={styles.nav}>
       <NavLink label="home" href="/" />
-      <NavLink label="about" href="/about/activity" />
       <NavLink label="work" href="/work/code" />
+      <NavLink label="about" href="/about/activity" />
     </nav>
   );
 }
