b4fe848ff806cf0ed90a038a69188197c899eda1
theycallmewolf <bruno.m.lobato@gmail.com>
2021-07-09 21:13:17 +0100

    'Aside' redesign

--- src/components/sections/Aside/index.tsx
+++ src/components/sections/Aside/index.tsx
@@ -1,23 +1,15 @@
-import { TangramCover } from '../../../assets/tangrams';
 import { AsideProps } from '../../../types';
 import { Nav } from '../../elements/Nav';
 import styles from './styles.module.scss';
 
-export function Aside({ imageURL, intro, link_list }: AsideProps): JSX.Element {
+export function Aside({ intro, link_list }: AsideProps): JSX.Element {
   return (
     <aside className={styles.aside}>
-      <div
-        className={styles.cover}
-        style={{
-          backgroundImage: `url(${imageURL})`
-        }}>
-        <TangramCover />
-      </div>
       <div className={styles.content}>
         <h1>{intro[0].title}</h1>
         <p className="lead">{intro[0].lead}</p>
-        <Nav customClass={styles.nav} link_list={link_list} />
       </div>
+      <Nav customClass={styles.nav} link_list={link_list} />
     </aside>
   );
 }
