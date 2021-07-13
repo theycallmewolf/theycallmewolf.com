4138d36d52b9b0d8806462d1e6546fb31702ea9f
theycallmewolf <bruno.m.lobato@gmail.com>
2021-07-13 21:03:25 +0100

    adds direct link to share image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
           <meta property="og:locale" content="en" />
           <meta property="og:type" content="website" />
           <meta property="og:site_name" content="https://www.theycallmewolf.com/" />
-          <meta property="og:image" content="assets/img/share.jpg" />
+          <meta property="og:image" content="https://i.ibb.co/8K9Ly43/share.jpg" />
           <meta property="og:image:secure_url" content="https://www.theycallmewolf.com/" />
           <meta property="og:image:alt" content="thumbnail" />
           <meta property="og:image:type" content="image/png" />
