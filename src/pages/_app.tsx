3f9ca3079a139f4bb1a95420a2b4f32d439e2041
theycallmewolf <bruno.m.lobato@gmail.com>
2021-07-23 00:02:11 +0100

    adds google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,31 @@ import 'swiper/components/effect-fade/effect-fade.min.css';
 import '../components/elements/Slider/styles.scss';
 
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import { ContactForm } from '../components/elements/ContactForm';
 import { AppProvider } from '../hooks';
+import { pageView } from '../utils/google-analytics';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageView(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <AppProvider>
       <Component {...pageProps} />
