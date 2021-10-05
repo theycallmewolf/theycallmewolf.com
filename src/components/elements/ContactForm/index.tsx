d31932f809180aac6af65a6ba3ff6ebbad13375c
theycallmewolf <bruno.m.lobato@gmail.com>
2021-10-05 11:50:59 +0100

    refactor ContactForm

--- src/components/elements/ContactForm/index.tsx
+++ src/components/elements/ContactForm/index.tsx
@@ -88,122 +88,120 @@ export function ContactForm(): JSX.Element {
   }
 
   return (
-    <>
-      <div className={`${styles.container} ${isOpen ? styles.open : undefined}`}>
-        <Button
-          type="button"
-          genre="fill"
-          customClass="call-to-action"
-          className={`${styles.button} ${isOpen ? styles.open : undefined}`}
-          onClick={() => toggleChat()}>
-          {isOpen ? <ICross /> : <IChat />}
-        </Button>
-        <div className={styles.content}>
-          <div className={styles.intro}>
-            <h2>
-              Say <br /> &quot;hello, wolf!&quot;
-            </h2>
-            <p>
-              More about me and my work?
-              <br /> Just drop a message.
-            </p>
-          </div>
-          <Formik
-            validationSchema={schema}
-            validateOnMount
-            validateOnBlur
-            validateOnChange
-            initialValues={initialValues}
-            onSubmit={(values, { setSubmitting, resetForm }) =>
-              handleSubmit({ values, setSubmitting, resetForm })
-            }>
-            {({ errors, resetForm, isSubmitting, isValid, validateForm }) => {
-              return (
-                <Form>
-                  {!isSubmitting ? (
-                    <>
-                      <div className={styles.formItem}>
-                        <label htmlFor="name">name</label>
-                        <Field type="text" id="name" name="name" />
-                        {errors.name && <span className={styles.error}>{errors.name}</span>}
-                      </div>
-                      <div className={styles.formItem}>
-                        <label htmlFor="email">email</label>
-                        <Field type="email" id="email" name="email" />
-                        {errors.email && <span className={styles.error}>{errors.email}</span>}
-                      </div>
-                      <div className={`${styles.formItem} ${styles.textarea}`}>
-                        <label htmlFor="message">message</label>
-                        <Field as="textarea" id="message" name="message" />
-                        {errors.message && <span className={styles.error}>{errors.message}</span>}
-                      </div>
-                      <div className={styles.buttonContainer}>
-                        <Button
-                          customClass={styles.resetBtn}
-                          type="reset"
-                          onClick={() => {
-                            resetForm();
-                            validateForm();
-                          }}>
-                          reset
-                        </Button>
-                        <Button
-                          customClass={styles.submitBtn}
-                          disabled={!isValid || isSubmitting}
-                          type="submit">
-                          send
-                        </Button>
-                      </div>
-                    </>
-                  ) : (
-                    <div className={styles.submitting}>
-                      <ILoading />
-                      <span>please wait...</span>
+    <div className={`${styles.container} ${isOpen ? styles.open : undefined}`}>
+      <Button
+        type="button"
+        genre="fill"
+        customClass="call-to-action"
+        className={`${styles.button} ${isOpen ? styles.open : undefined}`}
+        onClick={() => toggleChat()}>
+        {isOpen ? <ICross /> : <IChat />}
+      </Button>
+      <div className={styles.content}>
+        <div className={styles.intro}>
+          <h2>
+            Say <br /> &quot;hello, wolf!&quot;
+          </h2>
+          <p>
+            More about me and my work?
+            <br /> Just drop a message.
+          </p>
+        </div>
+        <Formik
+          validationSchema={schema}
+          validateOnMount
+          validateOnBlur
+          validateOnChange
+          initialValues={initialValues}
+          onSubmit={(values, { setSubmitting, resetForm }) =>
+            handleSubmit({ values, setSubmitting, resetForm })
+          }>
+          {({ errors, resetForm, isSubmitting, isValid, validateForm }) => {
+            return (
+              <Form>
+                {!isSubmitting ? (
+                  <>
+                    <div className={styles.formItem}>
+                      <label htmlFor="name">name</label>
+                      <Field type="text" id="name" name="name" />
+                      {errors.name && <span className={styles.error}>{errors.name}</span>}
                     </div>
-                  )}
-                </Form>
-              );
-            }}
-          </Formik>
-          <div className={styles.recaptchaContainer}>
-            <ReCAPTCHA
-              ref={recaptchaRef}
-              size="invisible"
-              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-              onChange={onChange}
-            />
-          </div>
-          <div className={`${styles.message} ${showMessage ? styles.show : undefined}`}>
-            <Wolf className={hasDarkMode ? styles.dark : undefined} />
-            {emailSent ? (
-              <>
-                <h2>Wolf say thanks!</h2>
-                <p>
-                  Thank you for your message.
-                  <br /> Looking forward to read it!
-                </p>
-              </>
-            ) : (
-              <>
-                <h2>Hmm...</h2>
-                <p>
-                  A problem sending your email? This is not a good impression, I know... Please send
-                  me an email to{' '}
-                  <a
-                    href="mailto:bruno@theycallmewolf.com?subject=I've%20visited%20your%20website%20Bruno!&body=Hi%20Bruno%2C%0D%0A..."
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    bruno@theycallmewolf.com
-                  </a>
-                </p>
-              </>
-            )}
-            <Button genre="outline" onClick={() => setIsOpen(false)}>
-              Close
-            </Button>
-          </div>
+                    <div className={styles.formItem}>
+                      <label htmlFor="email">email</label>
+                      <Field type="email" id="email" name="email" />
+                      {errors.email && <span className={styles.error}>{errors.email}</span>}
+                    </div>
+                    <div className={`${styles.formItem} ${styles.textarea}`}>
+                      <label htmlFor="message">message</label>
+                      <Field as="textarea" id="message" name="message" />
+                      {errors.message && <span className={styles.error}>{errors.message}</span>}
+                    </div>
+                    <div className={styles.buttonContainer}>
+                      <Button
+                        customClass={styles.resetBtn}
+                        type="reset"
+                        onClick={() => {
+                          resetForm();
+                          validateForm();
+                        }}>
+                        reset
+                      </Button>
+                      <Button
+                        customClass={styles.submitBtn}
+                        disabled={!isValid || isSubmitting}
+                        type="submit">
+                        send
+                      </Button>
+                    </div>
+                  </>
+                ) : (
+                  <div className={styles.submitting}>
+                    <ILoading />
+                    <span>please wait...</span>
+                  </div>
+                )}
+              </Form>
+            );
+          }}
+        </Formik>
+        <div className={styles.recaptchaContainer}>
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            size="invisible"
+            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+            onChange={onChange}
+          />
+        </div>
+        <div className={`${styles.message} ${showMessage ? styles.show : undefined}`}>
+          <Wolf className={hasDarkMode ? styles.dark : undefined} />
+          {emailSent ? (
+            <>
+              <h2>Wolf say thanks!</h2>
+              <p>
+                Thank you for your message.
+                <br /> Looking forward to read it!
+              </p>
+            </>
+          ) : (
+            <>
+              <h2>Hmm...</h2>
+              <p>
+                A problem sending your email? This is not a good impression, I know... Please send
+                me an email to{' '}
+                <a
+                  href="mailto:bruno@theycallmewolf.com?subject=I've%20visited%20your%20website%20Bruno!&body=Hi%20Bruno%2C%0D%0A..."
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  bruno@theycallmewolf.com
+                </a>
+              </p>
+            </>
+          )}
+          <Button genre="outline" onClick={() => setIsOpen(false)}>
+            Close
+          </Button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
