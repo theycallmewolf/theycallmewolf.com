8898c2c2dec6044bee044e5cf8c2784bd5f3624f
theycallmewolf <bruno.m.lobato@gmail.com>
2021-10-05 12:52:42 +0100

    adds send-sms api endpoint using Twilio

---
+++ src/pages/api/send-sms.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Twilio from 'twilio';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  // Download the helper library from https://www.twilio.com/docs/node/install
+  // Find your Account SID and Auth Token at twilio.com/console
+  // and set the environment variables. See http://twil.io/secure
+  const { phone, message } = req.query;
+
+  if (!phone || !message)
+    return res.status(500).json({ message: 'no phone number and message received' });
+
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = Twilio(accountSid, authToken);
+
+  const messageSID = client.messages
+    .create({
+      body: `${message}`,
+      from: process.env.TWILIO_PHONE,
+      to: `+351${phone}`
+    })
+    .then((message) => {
+      return message.sid;
+    })
+    .catch((err) => console.log(err));
+
+  return res.status(200).json({ messageSID });
+};
