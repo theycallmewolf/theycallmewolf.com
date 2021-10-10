ab992c07aec3e1946ce26a56b4ff4da1ca75b1b2
theycallmewolf <bruno.m.lobato@gmail.com>
2021-10-10 20:42:06 +0100

    next-pwa config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['i.scdn.co']
+  },
+  pwa: {
+    dest: 'public'
   }
-};
+});
