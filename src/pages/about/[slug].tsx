0ce778ed58ee3adb9ac18e44b8ffb542141ef5e4
theycallmewolf <bruno.m.lobato@gmail.com>
2021-11-01 20:28:54 +0000

    test: adds 'notFound' to 'getServerSideProps'

--- src/pages/about/[slug].tsx
+++ src/pages/about/[slug].tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Header, Aside } from 'components/sections';
+import { Header, Aside, Footer } from 'components/sections';
 import { GetServerSideProps } from 'next';
 import { getIntro } from 'services/prismic';
 import { AboutProps } from '../../types';
@@ -16,6 +16,8 @@ export default function About({ intro, link_list }: AboutProps): JSX.Element {
       </Head>
       <Header />
       <Aside intro={intro} link_list={link_list} imageURL="/assets/img/cover-about.jpg" />
+      <main></main>
+      <Footer />
     </>
   );
 }
@@ -25,6 +27,12 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const introList = await getIntro({ area: 'about' });
   const link_list = introList.map((item) => item.link_list).flat();
   const intro = introList.filter(({ title }) => title === slug);
+
+  if (!intro || !introList) {
+    return {
+      notFound: true
+    };
+  }
 
   return {
     props: {
