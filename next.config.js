50d17a4f72fa48afca00ebb17a18e441e5a6ed84
theycallmewolf <bruno.m.lobato@gmail.com>
2021-11-01 18:33:40 +0000

    updates pwa settings in next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = withPWA({
   },
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development'
+    disable: process.env.NODE_ENV === 'development',
+    buildExcludes: [/middleware-manifest.json$/]
   }
 });
