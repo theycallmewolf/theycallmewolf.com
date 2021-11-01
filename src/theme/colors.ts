0022577bfbe3db51565e7bb8543f8880d032fcdb
theycallmewolf <bruno.m.lobato@gmail.com>
2021-11-01 21:59:17 +0000

    adds 'colors' tokens

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  POLAR_WHITE: '#F5F5F5',
+  IRIDIUM_WHITE: '#F0F0F0',
+  SILVER_ARROW: '#d7d7d7',
+  NEUTRAL_GREY: '#3f3f3f',
+  COSMOS_BLACK: '#141f23',
+  NIGHT_BLACK: '#111517',
+  CANDY_RED: '#D20120'
+};
