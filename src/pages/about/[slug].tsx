f3a462b369af174f8e4216e1c2e2cca653cbcc82
theycallmewolf <bruno.m.lobato@gmail.com>
2021-11-01 20:23:55 +0000

    test 'about' page with Aside component

--- src/pages/about/[slug].tsx
+++ src/pages/about/[slug].tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
-import { Header } from 'components/sections';
+import { Header, Aside } from 'components/sections';
+import { GetServerSideProps } from 'next';
+import { getIntro } from 'services/prismic';
+import { AboutProps } from '../../types';
 
-export default function About(): JSX.Element {
+export default function About({ intro, link_list }: AboutProps): JSX.Element {
   return (
     <>
       <Head>
@@ -12,12 +15,21 @@ export default function About(): JSX.Element {
         />
       </Head>
       <Header />
-      <h1>
-        <br />
-        <br />
-        <br />
-        About Wolf
-      </h1>
+      <Aside intro={intro} link_list={link_list} imageURL="/assets/img/cover-about.jpg" />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { slug } = params;
+  const introList = await getIntro({ area: 'about' });
+  const link_list = introList.map((item) => item.link_list).flat();
+  const intro = introList.filter(({ title }) => title === slug);
+
+  return {
+    props: {
+      intro,
+      link_list
+    }
+  };
+};
