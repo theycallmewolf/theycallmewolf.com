756f8ebf06311a20292461793c023e8235af523f
theycallmewolf <bruno.m.lobato@gmail.com>
2021-11-01 20:44:19 +0000

    test: 'about' page with static props

--- src/pages/about/[slug].tsx
+++ src/pages/about/[slug].tsx
@@ -1,11 +1,41 @@
 import Head from 'next/head';
-import { Header, Aside, Footer } from 'components/sections';
+import { ServicesSVG } from 'assets/services';
+import { CardBody, CardHeader, DefaultCard, Graph, GraphicCard } from 'components/elements';
+import { Aside, CardList, Footer, Header } from 'components/sections';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { getIntro } from 'services/prismic';
-import { AboutProps } from '../../types';
+import {
+  getActivityContent,
+  getCareerContent,
+  getEducationContent,
+  getIntro,
+  getSkillsContent
+} from 'services/prismic';
+import { AboutProps } from 'types';
+import { useEffect } from 'react';
+import { useTheme } from 'hooks/useTheme';
+import { useRouter } from 'next/router';
+import styles from './styles.module.scss';
 
-export default function About({ link_list }: AboutProps): JSX.Element {
-  console.log({ link_list });
+export default function About({
+  intro,
+  link_list,
+  activity,
+  career,
+  education,
+  skills
+}: AboutProps): JSX.Element {
+  const router = useRouter();
+  const { slug } = router.query;
+  const { getTheme } = useTheme();
+
+  useEffect(getTheme);
+
+  useEffect(() => {
+    const activeArea = ['activity', 'skills', 'career', 'education'].filter(
+      (area) => area === slug
+    );
+    activeArea.length === 0 && router.push('/');
+  }, [router, slug]);
   return (
     <>
       <Head>
@@ -16,8 +46,66 @@ export default function About({ link_list }: AboutProps): JSX.Element {
         />
       </Head>
       <Header />
-      {/* <Aside intro={intro} link_list={link_list} imageURL="/assets/img/cover-about.jpg" /> */}
-      <main></main>
+      <Aside intro={intro} link_list={link_list} imageURL="/assets/img/cover-about.jpg" />
+      <main>
+        <CardList slug={slug}>
+          {slug === 'skills' &&
+            skills.map(({ id, title, description, graphs }) => (
+              <GraphicCard key={id} customClass={styles.card}>
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <div className={styles.graphList}>
+                  {graphs.map((graph) => (
+                    <Graph title={graph.title} percentage={graph.percentage} key={graph.id} />
+                  ))}
+                </div>
+              </GraphicCard>
+            ))}
+
+          {slug === 'activity' &&
+            activity.map(({ id, icon, title, description }) => (
+              <DefaultCard key={id}>
+                <CardHeader>
+                  <ServicesSVG icon={icon} />
+                </CardHeader>
+                <CardBody customClass={styles.card}>
+                  <h2>{title}</h2>
+                  <p>{description}</p>
+                </CardBody>
+              </DefaultCard>
+            ))}
+
+          {slug === 'career' &&
+            career.map(({ id, logo_svg, year_start, year_end, title, description }) => (
+              <GraphicCard customClass={styles.card} key={id}>
+                <div
+                  className={styles.svgContainer}
+                  dangerouslySetInnerHTML={{ __html: logo_svg }}
+                />
+                <span className={styles.center}>
+                  <p className={styles.date}>{`${year_start} - ${year_end || 'today'}`}</p>
+                  <h2>{title}</h2>
+                  <p>{description}</p>
+                </span>
+              </GraphicCard>
+            ))}
+
+          {slug === 'education' &&
+            education.map(({ id, logo_svg, year_start, year_end, title, description }) => (
+              <GraphicCard customClass={styles.card} key={id}>
+                <div
+                  className={styles.svgContainer}
+                  dangerouslySetInnerHTML={{ __html: logo_svg }}
+                />
+                <span className={styles.center}>
+                  <p className={styles.date}>{`${year_start} - ${year_end}`}</p>
+                  <h2>{title}</h2>
+                  <p>{description}</p>
+                </span>
+              </GraphicCard>
+            ))}
+        </CardList>
+      </main>
       <Footer />
     </>
   );
@@ -34,7 +122,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params;
   const introList = await getIntro({ area: 'about' });
   const link_list = introList.map((item) => item.link_list).flat();
-  // const intro = introList.filter(({ title }) => title === slug);
+  const intro = introList.filter(({ title }) => title === slug);
+  const activity = await getActivityContent();
+  const career = await getCareerContent();
+  const education = await getEducationContent();
+  const skills = await getSkillsContent();
 
   if (!link_list) {
     return {
@@ -44,7 +136,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   return {
     props: {
-      link_list
+      intro,
+      link_list,
+      activity,
+      career,
+      education,
+      skills
     },
     revalidate: 60 * 60 * 24 // 24 hours
   };
