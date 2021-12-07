f3b646aae5c34d96f70fa06d5570ff74c254f74a
theycallmewolf <bruno.m.lobato@gmail.com>
2021-12-07 19:15:38 +0000

    refactor 'NavLink' to 'NavButton'

--- src/components/sections/Header/Nav/NavButton/index.tsx
+++ src/components/sections/Header/Nav/NavButton/index.tsx
@@ -3,28 +3,26 @@ import { useRouter } from 'next/router';
 
 import styles from './styles.module.scss';
 
-interface NavLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
+interface NavButtonProps extends React.HTMLAttributes<HTMLAnchorElement> {
   href: string;
   label: string;
 }
-export function NavLink({ href, label }: NavLinkProps): JSX.Element {
+export function NavButton({ href, label }: NavButtonProps): JSX.Element {
   const router = useRouter();
   const { toggleNav } = useNav();
 
-  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
+  const handleClick = () => {
     router.push(href);
     toggleNav();
   };
 
   return (
-    <a
-      href={href}
-      className={`${styles.link} ${
+    <button
+      className={`${styles.button} ${
         router.asPath.split('/')[1] === href.split('/')[1] ? styles.active : undefined
       }`}
       onClick={handleClick}>
       {label}
-    </a>
+    </button>
   );
 }
--- src/components/sections/Header/Nav/index.tsx
+++ src/components/sections/Header/Nav/index.tsx
@@ -1,12 +1,12 @@
-import { NavLink } from './NavLink';
+import { NavButton } from './NavButton';
 import styles from './styles.module.scss';
 
 export function Nav(): JSX.Element {
   return (
     <nav className={styles.nav}>
-      <NavLink label="home" href="/" />
-      <NavLink label="work" href="/work/code" />
-      <NavLink label="about" href="/about/activity" />
+      <NavButton label="home" href="/" />
+      <NavButton label="work" href="/work/code" />
+      <NavButton label="about" href="/about/activity" />
     </nav>
   );
 }
