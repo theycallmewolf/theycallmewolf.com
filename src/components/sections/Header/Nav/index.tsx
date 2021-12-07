646697823df421e5f17769d83658cb87bb9bf795
theycallmewolf <bruno.m.lobato@gmail.com>
2021-12-07 19:15:46 +0000

    adds link to store

--- src/components/sections/Header/Nav/index.tsx
+++ src/components/sections/Header/Nav/index.tsx
@@ -7,6 +7,7 @@ export function Nav(): JSX.Element {
       <NavButton label="home" href="/" />
       <NavButton label="work" href="/work/code" />
       <NavButton label="about" href="/about/activity" />
+      <NavButton label="store" href="https://theycallmewolf.bigcartel.com/" />
     </nav>
   );
 }
