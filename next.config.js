ccd8903e18a78ddded85d5319b89d5f1f5d7f535
theycallmewolf <bruno.m.lobato@gmail.com>
2022-03-15 21:59:25 +0000

    Configs `bundle-analyzer`

--- next.config.js
+++ next.config.js
@@ -13,3 +13,10 @@ module.exports = withPWA({
     buildExcludes: [/middleware-manifest\.json$/]
   }
 });
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer({});
