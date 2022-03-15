23c6020dc1b4d5ff46dcd5f6dc41f3f0d9529b36
theycallmewolf <bruno.m.lobato@gmail.com>
2022-03-15 17:09:29 +0000

    Refactor `Slider` component

--- src/components/elements/Slider/index.tsx
+++ src/components/elements/Slider/index.tsx
@@ -1,3 +1,5 @@
+import { IPlus } from 'assets/icons';
+import { Button } from 'components/elements';
 import { useTheme } from 'hooks/useTheme';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
@@ -9,18 +11,18 @@ import { CustomLink } from '../Link';
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, EffectFade]);
 
-export function Slider({
+export const Slider: React.FC<SliderProps> = ({
   slides,
   testimonials,
   contentType,
   hasLink,
-  hasIcon,
-  additionalClass = '',
+  className = '',
   ...props
-}: SliderProps): JSX.Element {
+}) => {
   const [slideNumber, setSlideNumber] = useState('01');
   const [slideList, setSlideList] = useState([]);
   const [testimonialList, setTestimonialList] = useState([]);
+  const [showCaption, setShowCaption] = useState(false);
 
   const { hasDarkMode } = useTheme();
 
@@ -29,40 +31,51 @@ export function Slider({
     setTestimonialList(testimonials);
   }, [slides, testimonials]);
 
-  function handleSlideNumber(sliderIndex: number) {
+  const handleSlideNumber = (sliderIndex: number) => {
     setSlideNumber(String(sliderIndex + 1).padStart(2, '0'));
-  }
+  };
 
   return (
-    <div className={`slider-container ${hasDarkMode && 'dark'} ${additionalClass}`} {...props}>
+    <div className={`slider-container ${hasDarkMode && 'dark'} ${className}`} {...props}>
       <Swiper
         spaceBetween={48}
         navigation
+        slidesPerView={1}
         grabCursor={true}
         loop={true}
         speed={1200}
         effect={contentType === 'testimonial' ? 'fade' : 'slide'}
         className={contentType === 'testimonial' && 'testimonial'}
-        breakpoints={{
-          320: {
-            slidesPerView: 1
-          },
-          1024: {
-            slidesPerView: contentType === 'image' ? 2 : 1,
-            spaceBetween: 24
-          }
-        }}
         onSlideChange={(swiper) => handleSlideNumber(swiper.activeIndex)}>
         {contentType === 'image' &&
           slideList.map(({ slider, project_date, slug }, i) => {
             const { image_large_2x, caption } = slider;
             return (
-              <SwiperSlide key={i} className={contentType === 'image' && 'shadow'}>
-                <figure>
+              <SwiperSlide key={i}>
+                <figure className={contentType === 'image' && 'shadow'}>
                   <Image src={image_large_2x} layout="fill" quality={90} />
-                  {caption && <figcaption>{`(${project_date}) ${caption}`}</figcaption>}
+
+                  {caption && (
+                    <figcaption className={showCaption && 'show'}>
+                      <span>{project_date}</span>
+                      <strong>{caption}</strong>
+                      {hasLink && (
+                        <CustomLink
+                          href={`work/project/${slug}`}
+                          label="visit project"
+                          className="link"
+                        />
+                      )}
+                    </figcaption>
+                  )}
                 </figure>
-                {hasLink && <CustomLink href={`work/project/${slug}`} hasIcon={hasIcon} />}
+                {caption && (
+                  <Button
+                    className={`show-caption-button  ${showCaption && 'rotate'}`}
+                    onClick={() => setShowCaption((state) => !state)}>
+                    <IPlus />
+                  </Button>
+                )}
               </SwiperSlide>
             );
           })}
@@ -88,4 +101,4 @@ export function Slider({
       <div className="current-slide">{slideNumber}</div>
     </div>
   );
-}
+};
