77536984baf5063451f45ac7bf23c518bd9c8f95
theycallmewolf <bruno.m.lobato@gmail.com>
2022-04-08 10:12:02 +0100

    updates forced revalidation

---
+++ revalidate.sh
@@ -0,0 +1,4 @@
+curl "https://www.theycallmewolf.com/api/revalidate" \
+-X POST \
+-H "Content-Type: application/json" \
+-d "[\"/work/project/abababab\"]"
--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,3 +1,7 @@
+/**
+ * @see https://youtu.be/kdXKz1UWc3E?t=1558
+ */
+
 export default async (req, res) => {
   for (const url of req.body) {
     await res.unstable_revalidate(url);
