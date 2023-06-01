09e5398a3fedd53264203dd9443f61ffa7e2470b
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-06-01 13:26:37 +0100

    Fix `next.config`

--- next.config.js
+++ next.config.js
@@ -1,32 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withSentryConfig } = require("@sentry/nextjs");
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa");
-
-/** @type {import('next').NextConfig} */
-module.exports = withPWA({
-  reactStrictMode: true,
-  images: {
-    domains: ["i.scdn.co", "images.prismic.io", "images.unsplash.com"],
-  },
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    buildExcludes: [/middleware-manifest\.json$/],
-  },
-});
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({});
+const withPWA = require("next-pwa");
+
+/** @type {import('next').NextConfig} */
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    images: {
+      domains: ["i.scdn.co", "images.prismic.io", "images.unsplash.com"],
+    },
+    pwa: {
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+      buildExcludes: [/middleware-manifest\.json$/],
+    },
+  })
+);
+
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(
   module.exports,
