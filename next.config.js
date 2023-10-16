b56be52230c92690607116aa6f4a376191c83c54
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-16 11:58:01 +0100

    Adds `million.js`

--- next.config.js
+++ next.config.js
@@ -1,32 +1,35 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // This file sets a custom webpack configuration to use your Next.js app
 
+const withPWA = require("next-pwa");
+const { withSentryConfig } = require("@sentry/nextjs");
+const million = require("million/compiler");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const withPWA = require("next-pwa");
-
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      domains: ["i.scdn.co", "images.prismic.io", "images.unsplash.com"],
-    },
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-      buildExcludes: [/middleware-manifest\.json$/],
-    },
-  })
+module.exports = million.next(
+  withBundleAnalyzer(
+    withPWA({
+      reactStrictMode: true,
+      images: {
+        domains: ["i.scdn.co", "images.prismic.io", "images.unsplash.com"],
+      },
+      pwa: {
+        dest: "public",
+        disable: process.env.NODE_ENV === "development",
+        buildExcludes: [/middleware-manifest\.json$/],
+      },
+      auto: true,
+    })
+  )
 );
 
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(
   module.exports,
