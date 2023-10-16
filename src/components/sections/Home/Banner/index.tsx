b1cf4139120418ae67d930c712b1261f61b9b81d
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-16 12:00:56 +0100

    Adds `million block` to `Banner`

--- src/components/sections/Home/Banner/index.tsx
+++ src/components/sections/Home/Banner/index.tsx
@@ -1,5 +1,6 @@
 import "react-typed/dist/animatedCursor.css";
 
+import { block } from "million/react";
 import { useEffect } from "react";
 
 import styles from "./banner.module.scss";
@@ -10,13 +11,13 @@ import { Tools } from "./components/Tools";
 import { DeviceProvider, useDevice } from "./useDevice";
 import { UnsplashProvider, useUnsplash } from "./useUnsplash";
 
-export const Banner: React.FC = () => (
+export const Banner: React.FC = block(() => (
   <UnsplashProvider>
     <DeviceProvider>
       <BannerComponent />
     </DeviceProvider>
   </UnsplashProvider>
-);
+));
 
 export default Banner;
 
